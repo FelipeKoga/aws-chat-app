64b10f432630bb6dc1cbd7fd6f453e95ac535b2e
koga <kosloski.fkoga@gmail.com>
2021-09-05 02:06:01 -0300

    refactor(user): add get user keys function in repository

--- src/functions/user/repository/impl/UserRepositoryImpl.ts
+++ src/functions/user/repository/impl/UserRepositoryImpl.ts
@@ -3,22 +3,24 @@ import User from '@shared/models/User';
 import { inject, injectable } from 'tsyringe';
 import { IUserRepository } from '../IUserRepository';
 
+function getUserKeys(email: string): { pk: string, sk: string } {
+    return {
+        pk: DynamoConstants.Keys.userPartitionKey + email,
+        sk: DynamoConstants.Keys.config,
+    };
+}
 @injectable()
 class UserRepositoryImpl implements IUserRepository {
     constructor(@inject('DynamoProvider') private dynamoResolver: DynamoProvider) { }
 
     get(email: string): Promise<User> {
-        return this.dynamoResolver.get<User>({
-            pk: DynamoConstants.Keys.userPartitionKey + email,
-            sk: DynamoConstants.Keys.config,
-        });
+        return this.dynamoResolver.get<User>(getUserKeys(email));
     }
 
     create(user: User): Promise<boolean> {
         return this.dynamoResolver.create({
             ...user,
-            pk: DynamoConstants.Keys.userPartitionKey + user.email,
-            sk: DynamoConstants.Keys.config,
+            ...getUserKeys(user.email),
         });
     }
 }
